1c2c6f5819acef0f4ad19d91e97be3b368b18935
Malgorzata Kacperczyk <g.ochaaa@gmail.com>
2023-07-03 16:13:19 +0200

    add lesson 17

---
+++ topics/01-async/01-async.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.clear();
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+
+
+console.log("1");
+
+setTimeout(() => console.log("2"), 2000);
+
+console.log("3");
